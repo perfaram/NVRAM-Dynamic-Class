b88807ffd91a00e6e894958089b731c8aaf74f2b
Perceval FARAMAZ <perfaram@windowslive.com>
2015-04-18 06:51:23 +0200

    Initial Commit

---
+++ DynamicNVRAM/DynamicNVRAM.h
@@ -0,0 +1,19 @@
+//
+//  DynamicNVRAM.h
+//  DynamicNVRAM
+//
+//  Created by Perceval FARAMAZ on 18/04/15.
+//  Copyright (c) 2015 faramaz. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+//! Project version number for DynamicNVRAM.
+FOUNDATION_EXPORT double DynamicNVRAMVersionNumber;
+
+//! Project version string for DynamicNVRAM.
+FOUNDATION_EXPORT const unsigned char DynamicNVRAMVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DynamicNVRAM/PublicHeader.h>
+
+
