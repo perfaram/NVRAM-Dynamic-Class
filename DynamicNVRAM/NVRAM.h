107bed7886233a3b718886c33898fc3157e0f32a
Perceval FARAMAZ <perfaram@windowslive.com>
2015-04-18 06:52:18 +0200

    Created main class

---
+++ DynamicNVRAM/NVRAM.h
@@ -0,0 +1,13 @@
+//
+//  NVRAM.h
+//  DynamicNVRAM
+//
+//  Created by Perceval FARAMAZ on 18/04/15.
+//  Copyright (c) 2015 faramaz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NVRAM : NSObject
+
+@end
---
+++ DynamicNVRAM/NVRAM.m
@@ -0,0 +1,13 @@
+//
+//  NVRAM.m
+//  DynamicNVRAM
+//
+//  Created by Perceval FARAMAZ on 18/04/15.
+//  Copyright (c) 2015 faramaz. All rights reserved.
+//
+
+#import "NVRAM.h"
+
+@implementation NVRAM
+
+@end
