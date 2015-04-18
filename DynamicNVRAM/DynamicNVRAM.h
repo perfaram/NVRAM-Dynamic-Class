b2a5a070f0407fee4ccee8824fed84cf5e89c357
Perceval FARAMAZ <perfaram@windowslive.com>
2015-04-18 07:20:32 +0200

    #importing Main Class

--- DynamicNVRAM/DynamicNVRAM.h
+++ DynamicNVRAM/DynamicNVRAM.h
@@ -16,4 +16,4 @@ FOUNDATION_EXPORT const unsigned char DynamicNVRAMVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <DynamicNVRAM/PublicHeader.h>
 
-
+#import <DynamicNVRAM/NVRam.h>
